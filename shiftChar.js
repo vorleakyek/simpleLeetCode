399d7b78cb18b8aee39cec27fe01758a47e86b20
Vorleak Yek <vorleakyek@gmail.com>
2025-10-26 16:40:10 -0700

    Implement shiftChars function for multiple characters
    Added function to shift multiple characters based on a key.

--- shiftChar.js
+++ shiftChar.js
@@ -1,3 +1,5 @@
+//One character 
+
 function shiftChar(dictionary, key, char) {
     const charIndex = dictionary.indexOf(char);
     const shiftedIndex = (charIndex + key) % dictionary.length; 
@@ -10,7 +12,29 @@ dictionary = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 key = 3
 char = "z"
 
-const output = result(dictionary, key, char)
-
+const output = shiftChar(dictionary, key, char)
 console.log(output)
+
+
+//multiple characters 
+function shiftChars(dictionary, key, chars) {
+
+    let shiftedChars = "";
+
+    for (let i=0; i<chars.length; i++) {
+        const charIndex = dictionary.indexOf(chars[i]);
+        const shiftedIndex = (charIndex + key) % dictionary.length;
+        shiftedChars += dictionary[shiftedIndex];
+    }
+     
+    return shiftedChars;
+} 
+
+dictionary = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+key = -3
+chars = "ABC"
+
+const output = shiftChars(dictionary, key, chars)
+console.log(output)
+
 
