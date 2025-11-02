01c7e0c1b6c14732a5ba9fe0822bce3155ce5ed1
Vorleak Yek <vorleakyek@gmail.com>
2025-11-01 20:33:59 -0700

    Optimize sorting of arrivals and departures

--- minGatesRequired.rb
+++ minGatesRequired.rb
@@ -1,13 +1,27 @@
 def min_gates_required(arrivals, departures)
-  sorted_arrivals = arrivals.sort
-  sorted_departures = departures.sort
+  arrivals.sort!
+  departures.sort! 
   
   # A flight arrives before the next departure → need an extra gate.
   # A flight departs before the next arrival → free one gate. 
   
+  i = j = gates = max_gates = 0
+  
+  while i < arrivals.length && j < departures.length
+    
+    if (arrivals[i] < departures[j]) 
+      gates += 1
+      max_gates = [max_gates, gates].max
+      i += 1
+    else 
+      gates -= 1
+      j += 1
+    end
+  end
+
+  return max_gates
   
 end 
-
 
 
 arrivals = [900, 940, 950, 1100, 1500, 1800]
