7805fdbedb31e53e328cef0cfbddf44ec27cbe51
Vorleak Yek <vorleakyek@gmail.com>
2025-11-15 14:38:11 -0800

    Implement remove_element method for array manipulation
    Added a new method to remove elements from an array based on a specified value, along with comments for parameters and return values.

--- remove_duplicate.rb
+++ remove_duplicate.rb
@@ -1,3 +1,30 @@
+# @param {Integer[]} nums
+# @param {Integer} val
+# @return {Integer}
+def remove_element(nums, val)
+    nums.reject! {|num| num == val}
+    nums.length
+end
+
+
+# @param {Integer[]} nums
+# @param {Integer} val
+# @return {Integer}
+def remove_element(nums, val)
+    ind = 0
+
+    while ind < nums.length
+        if nums[ind] == val
+            nums.slice!(ind)
+        else 
+            ind += 1 
+        end
+    end
+
+    nums.length
+end
+
+
 def remove_duplicate(nums) 
   
   i = 0 
