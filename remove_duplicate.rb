51830b945645125f997122b68154ab98e9c2debd
Vorleak Yek <vorleakyek@gmail.com>
2025-11-17 09:21:11 -0800

    Refactor remove_duplicates method in remove_duplicate.rb
    Refactored remove_duplicates method to improve performance and removed duplicate implementation.

--- remove_duplicate.rb
+++ remove_duplicate.rb
@@ -1,3 +1,45 @@
+#https://leetcode.com/problems/remove-duplicates-from-sorted-array/?envType=study-plan-v2&envId=top-interview-150
+
+# @param {Integer[]} nums
+# @return {Integer}
+def remove_duplicates(nums)
+  return 0 if nums.empty?
+
+  write = 1
+
+  (1...nums.length).each do |read|
+    if nums[read] != nums[read - 1]
+      nums[write] = nums[read]
+      write += 1
+    end
+  end
+
+  # Optional: truncate the array if needed
+  nums.slice!(write..-1)
+
+  write
+end
+
+# # @param {Integer[]} nums
+# # @return {Integer}
+def remove_duplicates(nums)
+
+    i = 1
+
+    while (i < nums.length)
+        if (nums[i] == nums[i-1])
+            nums.slice!(i,1)
+        else
+            i += 1 
+        end
+    end 
+
+    nums.length
+
+end
+
+
+
 # @param {Integer[]} nums
 # @param {Integer} val
 # @return {Integer}
