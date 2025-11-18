816ab4d74d80c6f3c5fbda2d6e0c585c2aaf9188
Vorleak Yek <vorleakyek@gmail.com>
2025-11-17 21:38:11 -0800

    Add roman_to_int function for numeral conversion
    Implemented a function to convert Roman numerals to integers.

--- roman_to_integer.rb
+++ roman_to_integer.rb
@@ -1,6 +1,36 @@
 # @param {String} s
 # @return {Integer}
 def roman_to_int(s)
+  val = {
+    "I" => 1,
+    "V" => 5,
+    "X" => 10,
+    "L" => 50,
+    "C" => 100,
+    "D" => 500,
+    "M" => 1000
+  }
+
+  total = 0
+  prev = 0
+
+  s.reverse.each_char do |char|
+    curr = val[char]
+    if curr < prev
+      total -= curr
+    else
+      total += curr
+    end
+    prev = curr
+  end
+
+  total
+end
+
+
+# @param {String} s
+# @return {Integer}
+def roman_to_int(s)
 
     value = 0
     ind = 0 
