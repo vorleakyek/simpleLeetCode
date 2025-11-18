326bca13772b8da302cf9c73149ec67e4c4346b0
Vorleak Yek <vorleakyek@gmail.com>
2025-11-17 19:49:03 -0800

    Update arr-rotation.rb

--- arr-rotation.rb
+++ arr-rotation.rb
@@ -1,3 +1,32 @@
+# https://leetcode.com/problems/rotate-array/submissions/1832911603/?envType=study-plan-v2&envId=top-interview-150
+# update in-place
+
+# @param {Integer[]} nums
+# @param {Integer} k
+# @return {Void} Do not return anything, modify nums in-place instead.
+def rotate(nums, k)
+
+    reverse(nums, 0, nums.length - 1)
+    reverse(nums, 0, k - 1)
+    reverse(nums, k , nums.length - 1)
+
+    nums
+ 
+end
+
+def reverse(nums, left, right)
+    while left < right
+        nums[left], nums[right] = nums[right], nums[left]
+        left += 1
+        right -= 1 
+    end
+end
+
+
+
+# create a new array 
+
+
 def array_rotation(arr, k)
   
   result = []
